c2a02d8ee5af70d77efeb21e6a91e4bd7e775c99
RomSribn <steeper.fest@gmail.com>
2020-05-25 22:10:37 +0300

    new config with client's credentials

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,57 +1,57 @@
 export default {
   widgets: [
     {
-      name: 'sanity-tutorials',
+      name: "sanity-tutorials",
       options: {
-        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
-      }
+        templateRepoId: "sanity-io/sanity-template-gatsby-portfolio",
+      },
     },
-    {name: 'structure-menu'},
+    { name: "structure-menu" },
     {
-      name: 'project-info',
+      name: "project-info",
       options: {
         __experimental_before: [
           {
-            name: 'netlify',
+            name: "netlify",
             options: {
               description:
-                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+                "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
               sites: [
                 {
-                  buildHookId: '5ebba8a995b7c44e195924b6',
-                  title: 'Sanity Studio',
-                  name: 'lightdev-portfolio-studio',
-                  apiId: '3e38d238-ea4d-430d-bac2-858588edf243'
+                  buildHookId: "5ecc1115c16d8435a150e4c6",
+                  title: "Sanity Studio",
+                  name: "lightdev-portfolio-studio",
+                  apiId: "3e38d238-ea4d-430d-bac2-858588edf243",
                 },
                 {
-                  buildHookId: '5ebba8aad4d5821f1aad0834',
-                  title: 'Portfolio Website',
-                  name: 'lightdev-portfolio',
-                  apiId: '0a4ed446-642b-46c5-a2df-fd7754a5ab70'
-                }
-              ]
-            }
-          }
+                  buildHookId: "5ecc12b751f1bcd7fa610e9a",
+                  title: "Portfolio Website",
+                  name: "lightdev-portfolio",
+                  apiId: "0a4ed446-642b-46c5-a2df-fd7754a5ab70",
+                },
+              ],
+            },
+          },
         ],
         data: [
           {
-            title: 'GitHub repo',
-            value: 'https://github.com/RomSribn/sanity-gatsby-portfolio',
-            category: 'Code'
+            title: "GitHub repo",
+            value: "https://github.com/LightDevGames/sanity-gatsby-portfolio",
+            category: "Code",
           },
           {
-            title: 'Frontend',
-            value: 'https://sanity-gatsby-portfolio-web-v6jyh9ct.netlify.app',
-            category: 'apps'
-          }
-        ]
-      }
+            title: "Frontend",
+            value: "https://lightdevgames.netlify.app",
+            category: "apps",
+          },
+        ],
+      },
     },
-    {name: 'project-users', layout: {height: 'auto'}},
+    { name: "project-users", layout: { height: "auto" } },
     {
-      name: 'document-list',
-      options: {title: 'Recent projects', order: '_createdAt desc', types: ['sampleProject']},
-      layout: {width: 'medium'}
-    }
-  ]
-}
+      name: "document-list",
+      options: { title: "Recent projects", order: "_createdAt desc", types: ["sampleProject"] },
+      layout: { width: "medium" },
+    },
+  ],
+};
