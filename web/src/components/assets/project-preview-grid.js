92db0af303a4066e28c72bacfe51ff64d59a0dc5
RomSribn <steeper.fest@gmail.com>
2020-06-19 18:05:57 +0300

    creating responsive image

--- web/src/components/assets/project-preview-grid.js
+++ web/src/components/assets/project-preview-grid.js
@@ -1,10 +1,10 @@
-import {Link, navigate} from 'gatsby'
-import React from 'react'
-import ProjectPreview from './project-preview'
+import { Link, navigate } from "gatsby";
+import React from "react";
+import ProjectPreview from "./project-preview";
 
-import styles from './styles/project-preview-grid.module.css'
+import styles from "./styles/project-preview-grid.module.css";
 
-function ProjectPreviewGrid (props) {
+function ProjectPreviewGrid(props) {
   return (
     <div className={styles.root} onClick={() => navigate(props.linkTo)}>
       {props.title && <h2 className={styles.headline}>{props.title}</h2>}
@@ -22,13 +22,13 @@ function ProjectPreviewGrid (props) {
         </div>
       )}
     </div>
-  )
+  );
 }
 
 ProjectPreviewGrid.defaultProps = {
-  title: '',
+  title: "",
   nodes: [],
-  browseMoreHref: ''
-}
+  browseMoreHref: ""
+};
 
-export default ProjectPreviewGrid
+export default ProjectPreviewGrid;
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import {graphql} from 'gatsby'
-import Container from '../containers/container'
-import GraphQLErrorList from '../components/graphql-error-list'
-import ProjectPreviewGrid from '../components/assets/project-preview-grid'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
-import Button from '../components/footer/Button.jsx'
-import {mapEdgesToNodes, filterOutDocsWithoutSlugs} from '../lib/helpers'
-import AboutBlock from '../components/about/AboutBlock.jsx'
-import {responsiveTitle1} from '../components/typography.module.css'
-import {center} from '../containers/styles/container.module.css'
+import React from "react";
+import { graphql } from "gatsby";
+import Container from "../containers/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import ProjectPreviewGrid from "../components/assets/project-preview-grid";
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
+import Button from "../components/footer/Button.jsx";
+import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from "../lib/helpers";
+import AboutBlock from "../components/about/AboutBlock.jsx";
+import { responsiveTitle1 } from "../components/typography.module.css";
+import { center } from "../containers/styles/container.module.css";
 
 export const query = graphql`
   query ArchivePageQuery {
@@ -29,8 +29,8 @@ export const query = graphql`
     }
     projects: allSanitySampleProject(
       limit: 12
-      sort: {fields: [publishedAt], order: DESC}
-      filter: {slug: {current: {ne: null}}, publishedAt: {ne: null}}
+      sort: { fields: [publishedAt], order: DESC }
+      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
     ) {
       edges {
         node {
@@ -51,24 +51,24 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
 const ArchivePage = props => {
-  const {data, errors} = props
-  const email = data && data.siteSettings.author && data.siteSettings.author.email
-  const projects = data && data.projects && mapEdgesToNodes(data.projects)
-  const aboutBlocks = data && data.aboutBlocks && mapEdgesToNodes(data.aboutBlocks)
+  const { data, errors } = props;
+  const email = data && data.siteSettings.author && data.siteSettings.author.email;
+  const projects = data && data.projects && mapEdgesToNodes(data.projects);
+  const aboutBlocks = data && data.aboutBlocks && mapEdgesToNodes(data.aboutBlocks);
   if (errors) {
     return (
       <Layout>
         <GraphQLErrorList errors={errors} />
       </Layout>
-    )
+    );
   }
-  const projectNodes = data && data.projects && projects.filter(filterOutDocsWithoutSlugs)
+  const projectNodes = data && data.projects && projects.filter(filterOutDocsWithoutSlugs);
   return (
     <Layout>
-      <SEO title='LightDev' />
+      <SEO title="LightDev" />
       <Container>
         <AboutBlock aboutBlocks={aboutBlocks} />
       </Container>
@@ -77,11 +77,11 @@ const ArchivePage = props => {
       </Container>
       <Container>
         <div className={center}>
-          <Button color='blue' text='Contact us' mailTo={email} />
+          <Button color="blue" text="Contact us" mailTo={email} />
         </div>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default ArchivePage
+export default ArchivePage;
