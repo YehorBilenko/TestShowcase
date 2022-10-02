31805a365fcc3ebee7350db138b83b60a91d68e5
Roman Sribnyi <Roman_Sribnyi@epesmall0004.technicolor.net>
2022-10-02 14:38:53 +0200

    fix(components): remove unusable code

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -2,16 +2,12 @@ import './Home.scss';
 import * as React from 'react';
 import { games } from './utils';
 import { AppContext } from 'App';
-import { home } from 'routes/variables';
 import { Slider } from './components/Slider';
-import { useLocation } from 'react-router-dom';
 import { MoreLinks } from '_assets/shared/MoreLinks';
 import { ScrollButton } from '_assets/shared/ScrollButton';
 
 export const Home: React.FC = () => {
   const { isClicked, toggleClicked } = React.useContext(AppContext);
-  const location = useLocation();
-  const isHome = location.pathname === home;
   return (
     <div className="home">
       <div className={`content ${isClicked && 'active'}`}>
