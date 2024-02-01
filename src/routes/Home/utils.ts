2ac069552b22391e70a0624ca8c27b7eee4701ae
BilenkoYehor <yabdarkapps@gmail.com>
2024-02-01 12:58:15 +0100

    Update utils.ts

--- src/routes/Home/utils.ts
+++ src/routes/Home/utils.ts
@@ -13,7 +13,7 @@ export const games: TGame[] = [
   {
     id: 1,
     title: 'Hooping Heads',
-    subTitle: '44m+ DOWNLOADS',
+    subTitle: '45m+ DOWNLOADS',
     googleStoreLink:
       'https://play.google.com/store/apps/details?id=com.lightdev.hoppingheads',
     appStoreLink:
@@ -23,7 +23,7 @@ export const games: TGame[] = [
   {
     id: 2,
     title: 'Weapon Survivor',
-    subTitle: '3m+ DOWNLOADS',
+    subTitle: '4m+ DOWNLOADS',
     googleStoreLink:
       'https://play.google.com/store/apps/details?id=com.lightdev.weaponsurvivor',
     appStoreLink:
